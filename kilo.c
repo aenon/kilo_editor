dba4555c1eee3a9e65a98a47c473691a31fa2bf2
Xilin Sun <s.sn.giraffe@gmail.com>
2017-05-19 16:34:52 -0700

    try printf c

--- kilo.c
+++ kilo.c
@@ -4,9 +4,12 @@
 // Ref: http://viewsourcecode.org/snaptoken/kilo/01.setup.html
 
 #include <unistd.h>
+#include <stdio.h>
 
 int main() {
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    printf("%c", c);
+  }
 	return 0;
 }
