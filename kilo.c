babda8195b6d3545ac7edd74aa4e383665830fef
Xilin Sun <s.sn.giraffe@gmail.com>
2017-09-14 16:22:07 -0700

    update kilo

--- kilo.c
+++ kilo.c
@@ -3,30 +3,9 @@
 // A text editor in text mode
 // Ref: http://viewsourcecode.org/snaptoken/kilo/01.setup.html
 
-#include <stdlib.h>
-#include <termios.h>
 #include <unistd.h>
-
-struct termios orig_termios;
-
-void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
-
-void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexot(disableRawMode);
-
-  struct termios raw = orig_termios;;
-  raw.c_lflag &= ~(ECHO);
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
 int main() {
-  enableRawMode();
-
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-	return 0;
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  return 0;
 }
