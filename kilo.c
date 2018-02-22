de61b005d1633085286385f8fa22c8ef9370de6c
Xilin Sun <s.sn.giraffe@gmail.com>
2018-02-22 13:51:20 -0800

    fix output by adding \r

--- kilo.c
+++ kilo.c
@@ -50,11 +50,11 @@ int main() {
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
       // prints the ASCII code for control characters
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } 
     else {
       // prints the ASCII code and the character itself
-      printf("%d (%c)\n", c, c);
+      printf("%d (%c)\r\n", c, c);
     }
   }
   return 0;
