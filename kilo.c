bb0453789b095ebcf214c1a3ca2f774832cd1e26
Xilin Sun <s.sn.giraffe@gmail.com>
2019-12-16 23:42:47 -0800

    enable raw mode

--- kilo.c
+++ kilo.c
@@ -7,11 +7,24 @@
  * make && ./kilo
  */
 
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+  struct termios raw;
+  // gets and sets terminal attributes
+  // to turn off echoing
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(void) {
-  // the starting point of the c program
+  enableRawMode();
+
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1);
+  
+  // reads to c; press 'q' to exit
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
